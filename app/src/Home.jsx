ebca3fe000f0af5dce5e1a840776c9171f827058
Equan P. <equan.p@gmail.com>
2024-04-10 16:33:26 +0700

    add audio recorder component

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -1,11 +1,50 @@
+import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
+
 export default function Home() {
+
+	const recorderControls = useAudioRecorder(
+		{
+			noiseSuppression: true,
+			echoCancellation: true,
+		},
+		(err) => console.table(err) // onNotAllowedOrFound
+	);
+	const addAudioElement = (blob) => {
+		const url = URL.createObjectURL(blob);
+		const audio = document.createElement('audio');
+		audio.src = url;
+		audio.controls = true;
+		document.body.appendChild(audio);
+
+		const formData = new FormData();
+		formData.append("file", blob, "audio.webm");
+
+		fetch('http://localhost:3000/upload', {
+			method: 'POST',
+			body: formData,
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log('Success:', data);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+	};
+
 	return (
 		<div className="hero min-h-screen bg-base-200">
 			<div className="hero-content text-center">
 				<div className="max-w-md">
-					<h1 className="text-5xl font-bold">Hello there</h1>
-					<p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-					<button className="btn btn-primary">Get Started</button>
+					<h1 className="text-5xl font-bold">Audio Note</h1>
+					<div className='flex justify-center items-center w-full my-32'>
+						<AudioRecorder
+							onRecordingComplete={(blob) => addAudioElement(blob)}
+							recorderControls={recorderControls}
+							showVisualizer={true}
+						/>
+					</div>
+					<br />
 				</div>
 			</div>
 		</div>
